f0d1403064ffda4378cbac1f55a2980f94957296
lingjianli <1255676103@qq.com>
2020-07-10 10:00:42 +0800

    finish 01.js

---
+++ coding/01.js
@@ -0,0 +1,42 @@
+/**
+ * 将罗马数字转成数字
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+  let hashNum = {
+      I: 1,
+      IV: 4,
+      V: 5,
+      IX: 9,
+      X: 10,
+      XL: 40,
+      L: 50,
+      XC: 90,
+      C: 100,
+      CD: 400,
+      D: 500,
+      CM: 900,
+      M: 1000,
+    },
+    ans = 0,
+    len = s.length;
+  for (let i = 0; i < len; ) {
+    if (i + 1 < len && hashNum[s.substring(i, i + 2)]) {
+      ans += hashNum[s.substring(i, i + 2)];
+      i += 2;
+      continue;
+    }
+    ans += hashNum[s.substring(i, i + 1)];
+    i++
+  }
+  return ans;
+};
+
+
+console.log(romanToInt("MCMXCIV"))
+console.log(romanToInt("LVIII"))
+console.log(romanToInt("IX"))
